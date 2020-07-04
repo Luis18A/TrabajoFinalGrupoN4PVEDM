439658bd57d2102217728ed871271a5f83d67c32
tont <tont@DESKTOP-ET74MV3>
2020-07-04 14:46:52 -0300

    Edicion y Eliminacion del usuario, sin html. Responsable: Aramayo Nahuel

--- src/main/java/ar/edu/unju/edm/service/IUsuarioServiceImp.java
+++ src/main/java/ar/edu/unju/edm/service/IUsuarioServiceImp.java
@@ -20,9 +20,39 @@ public class IUsuarioServiceImp implements IUsuarioService{
 		// TODO Auto-generated method stub
 		iUsuarioRepository.save(usuario);
 	}
+	
+	@Override
+	public Usuario modificar(Usuario usuario) throws Exception{
+		Usuario guardarUsuario= encontrarUsuario(usuario.idUsuario);
+		mapearUsuario(usuario, guardarUsuario);
+		return iUsuarioRepository.save(guardarUsuario);
+	}
+	public void mapearUsuario(Usuario desde, Usuario hacia) {
+		hacia.setNombreReal(desde.getNombreReal());
+		hacia.setApellidoReal(desde.getApellidoReal());
+		hacia.setNombreUsuario(desde.getNombreUsuario());
+		hacia.setPassword(desde.getPassword());
+		hacia.setTipoUsuario(desde.getTipoUsuario());
+	}
 
 	@Override
 	public List<Usuario> obtenerUsuarios() {
 		return iUsuarioRepository.obtenerUsuarios();
+	}
+	@Override
+	public Usuario encontrarUsuario(Long idUsuario) throws Exception{
+		return iUsuarioRepository.findById(idUsuario).orElseThrow(()-> new Exception("El usuario no existe"));
+	}
+
+	@Override
+	public void eliminar(Long idUsuario) {
+		// TODO Auto-generated method stub
+		iUsuarioRepository.deleteById(idUsuario);
+	}
+	
+	@Override
+	public Iterable<Usuario> listarTodos() {
+		// TODO Auto-generated method stub
+		return iUsuarioRepository.findAll();
 	}
 }
